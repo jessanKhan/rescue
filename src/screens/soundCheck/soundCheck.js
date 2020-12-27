ea846d9bd6a5e4609f691c474cc1309a5e25585d
jessanKhan <39450312+jessanKhan@users.noreply.github.com>
2020-12-28 02:09:00 +0600

    update

---
+++ src/screens/soundCheck/soundCheck.js
@@ -0,0 +1,19 @@
+// import React, {Component} from 'react';
+// import {View, Text} from 'react-native';
+
+// class SoundCheck extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+
+//   render() {
+//     return (
+//       <View>
+//         <Text> soundCheck </Text>
+//       </View>
+//     );
+//   }
+// }
+
+// export default SoundCheck;
